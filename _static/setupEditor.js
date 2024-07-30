f7727d734ab4636cf9bf0d8bb48b9148493a56bb
reneaas <reneaas@users.noreply.github.com>
2024-07-30 17:11:12 +0000

    deploy: 61ebdb3e8d039a881c011fdff5bb9bed95dd2cfe

--- _static/setupEditor.js
+++ _static/setupEditor.js
@@ -9,14 +9,56 @@ async function initializePyodide() {
 
 // Setter opp code editor med code mirror
 async function setupEditor(pyodide, editorId, buttonId, outputId) {
+    const lightTheme = "solarized";
+    const darkTheme = "midnight";
+
+    // function getCurrentTheme() {
+    //     return window.matchMedia('(prefers-color-scheme: dark)').matches ? darkTheme : lightTheme;
+    // }
+
+    function getCurrentTheme() {
+        const mode = document.documentElement.getAttribute('data-mode');
+        // console.log("Mode: " + mode); // Debugging line for check `mode` value
+    
+        if (mode === 'dark') {
+            return darkTheme;
+        } else if (mode === 'light') {
+            return lightTheme;
+        } else if (mode === 'auto') {
+            const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+            return prefersDarkScheme ? darkTheme : lightTheme;
+        }
+    }
+
+    console.log("Current theme: " + getCurrentTheme());
+    
     let editor = CodeMirror.fromTextArea(document.getElementById(editorId), {
         mode: "python",
         lineNumbers: true,
-        theme: "midnight", // Other themes at https://codemirror.net/5/demo/theme.html#default
+        theme: getCurrentTheme(), // Other themes at https://codemirror.net/5/demo/theme.html#default
         tabSize: 4,
         indentUnit: 4,
         fontSize: 30,
     });
+
+    const observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            if (mutation.attributeName === 'data-mode') {
+                // console.log('data-mode attribute changed'); // Debugging line to check attribute change
+                editor.setOption('theme', getCurrentTheme());
+            }
+        });
+    });
+
+
+    // Start observing the document's data-mode attribute for changes
+    observer.observe(document.documentElement, {
+        attributes: true,
+        attributeFilter: ['data-mode']
+    });
+
+    // Initial theme setup
+    editor.setOption('theme', getCurrentTheme());
     
     let runButton = document.getElementById(buttonId);
     let output = document.getElementById(outputId);
