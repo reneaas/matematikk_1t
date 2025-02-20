f100c997401d2ca95e190906fb44ef81e75f3613
reneaas <reneaas@uio.no>
2025-02-20 01:34:43 +0100

    update

--- _static/interactiveCode/workerManager.js
+++ _static/interactiveCode/workerManager.js
@@ -3,57 +3,35 @@
 class WorkerManager {
     static instance = null;
 
-    // static getInstance(preloadPackages = null) {
-    //     if (!WorkerManager.instance) {
-    //         // Include 'matplotlib' in the default preloadPackages
-    //         const defaultPreloadPackages = ['casify', 'matplotlib', 'micropip', 'numpy', 'scipy', 'sympy'];
-    //         const combinedPreloadPackages = preloadPackages
-    //             ? [...defaultPreloadPackages, ...preloadPackages]
-    //             : defaultPreloadPackages;
-    //         WorkerManager.instance = new WorkerManager(combinedPreloadPackages);
-    //     } else {
-    //         // If preloadPackages is provided later, ensure the packages are loaded
-    //         if (preloadPackages) {
-    //              // Include matplotlib in the set of packages.
-    //              const packagesToLoad = ['casify', 'matplotlib', 'micropip', 'numpy', 'scipy', 'sympy', ...preloadPackages];
-    //              WorkerManager.instance.loadPackages(packagesToLoad);
-    //         }
-    //     }
-    //     return WorkerManager.instance;
-    // }
-
     static getInstance(preloadPackages = null) {
         if (!WorkerManager.instance) {
-            // Default PYODIDE packages (no custom packages here)
+            // Default PYODIDE packages (micropip is needed for custom packages)
             const defaultPreloadPackages = ['matplotlib', 'numpy', 'scipy', 'sympy', 'micropip'];
             const combinedPreloadPackages = Array.from(new Set(preloadPackages ? [...defaultPreloadPackages, ...preloadPackages] : defaultPreloadPackages));
             WorkerManager.instance = new WorkerManager(combinedPreloadPackages);
         } else if (preloadPackages) {
-            // Only load packages that are NOT already loaded.
+             // Only load packages that are NOT already loaded.
             const packagesToLoad = preloadPackages.filter(pkg => !WorkerManager.instance.loadedPackages.has(pkg));
-             const combinedPreloadPackages = Array.from(new Set(['matplotlib', 'numpy', 'scipy', 'sympy', ...packagesToLoad])); // Pyodide packages
+            const combinedPreloadPackages = Array.from(new Set(['matplotlib', 'numpy', 'scipy', 'sympy', 'micropip', ...packagesToLoad]));
             if (combinedPreloadPackages.length > 0) {
-    
                 WorkerManager.instance.loadPackages(combinedPreloadPackages);
             }
         }
         return WorkerManager.instance;
     }
 
-
-    constructor(preloadPackages = null) { // Default to preloading matplotlib
+    constructor(preloadPackages = []) { // Corrected default
         if (WorkerManager.instance) {
             return WorkerManager.instance;
         }
 
         this.worker = null;
-        this.callbacks = {}; // For managing callbacks with message IDs
+        this.callbacks = {};
         this.preloadPackages = preloadPackages;
         this.loadedPackages = new Set();
-        this.packageLoadPromises = {}; // Map of packageRequestId to {resolve, reject, packages}
+        this.packageLoadPromises = {};
         console.log("Preload packages in WorkerManager:", this.preloadPackages);
 
-        // Create a promise that resolves when the worker is ready and preloadPackages are loaded
         this.workerReadyPromise = new Promise((resolve, reject) => {
             this.workerReadyResolve = resolve;
             this.workerReadyReject = reject;
@@ -86,24 +64,32 @@ async function installPackages(pyodide, packages) {
         await pyodide.loadPackage("micropip"); // Load micropip
         const micropip = pyodide.pyimport("micropip");
         await micropip.install(packages);
-        await micropip.install("casify");
-        await micropip.install("plotmath");
-        await micropip.install("signchart");
     }
 }
 
 onmessage = async (event) => {
     const messageId = event.data.messageId;
+
     if (event.data.type === 'init') {
+        const { preloadPackages } = event.data; // Receive preloadPackages
         const pyodide = await pyodideReadyPromise;
-        await pyodide.loadPackage("micropip");
-        // await installPackages(pyodide, event.data.preloadPackages || []);
-        
         initialGlobals = new Set(pyodide.globals.keys());
-        postMessage(JSON.stringify({ type: 'initReady' }));
+
+        // Separate Pyodide packages from PyPI packages
+        const pyodidePackages = preloadPackages.filter(pkg => ['matplotlib', 'numpy', 'scipy', 'sympy', 'micropip'].includes(pkg));
+        const pypiPackages = preloadPackages.filter(pkg => !['matplotlib', 'numpy', 'scipy', 'sympy', 'micropip'].includes(pkg));
+
+        // Load Pyodide packages
+        if (pyodidePackages.length > 0) {
+            await pyodide.loadPackage(pyodidePackages);
+        }
+
+        // Install PyPI packages using micropip
+        await installPackages(pyodide, pypiPackages);
+
+        postMessage(JSON.stringify({ type: 'initReady' })); // Send AFTER preloading
     }
 
-    
     if (event.data.type === 'runCode') {
         const { code } = event.data;
         try {
@@ -114,7 +100,6 @@ onmessage = async (event) => {
             const pyCode = \`
 import sys
 import json
-import micropip
 import io
 import base64
 from js import postMessage
@@ -144,21 +129,27 @@ sys.stderr = PyConsole("\${messageId}")
 def show_override(messageId):
     buf = io.BytesIO()
     plt.savefig(buf, format='png')
-
+    fig = plt.gcf()
+    width_in = fig.get_figwidth()
+    height_in = fig.get_figheight()
+    dpi = fig.get_dpi()
+    width_px = int(width_in * dpi)
+    height_px = int(height_in * dpi)
     buf.seek(0)
     image_base64 = base64.b64encode(buf.read()).decode('utf-8')
     postMessage(json.dumps({
         'type': 'plot',
         'data': image_base64,
-        'messageId': messageId
+        'messageId': messageId,
+        'width': width_px,
+        'height': height_px
     }))
-    plt.clf() # Clear the figure after sending
+    plt.clf()
 
 plt.show = lambda: show_override("\${messageId}")
 \`;
 
             await pyodide.runPythonAsync(pyCode);
-
             await pyodide.runPythonAsync(code);
             postMessage(JSON.stringify({ type: 'executionComplete', messageId }));
         } catch (err) {
@@ -171,22 +162,18 @@ plt.show = lambda: show_override("\${messageId}")
         try {
             const pyodide = await pyodideReadyPromise;
             console.log("Loading packages:", packages);
-            const filteredPackages = packages.filter(pkg => pkg !== "casify");
-            await pyodide.loadPackage(filteredPackages);
 
-            // Custom installation of casify package if present in the package list.
-            if (packages.includes('casify')) {
-                await pyodide.loadPackage("micropip");
-                await pyodide.runPythonAsync("import micropip; await micropip.install('casify')");
+            // Separate Pyodide packages from PyPI packages
+            const pyodidePackages = packages.filter(pkg => ['matplotlib', 'numpy', 'scipy', 'sympy', 'micropip'].includes(pkg));
+            const pypiPackages = packages.filter(pkg => !['matplotlib', 'numpy', 'scipy', 'sympy', 'micropip'].includes(pkg));
+
+            // Load Pyodide packages directly
+            if (pyodidePackages.length > 0) {
+                await pyodide.loadPackage(pyodidePackages);
             }
-            if (packages.includes('plotmath')) {
-                await pyodide.loadPackage("micropip");
-                await pyodide.runPythonAsync("import micropip; await micropip.install('plotmath')");
-            }
-            if (packages.includes('signchart')) {
-                await pyodide.loadPackage("micropip");
-                await pyodide.runPythonAsync("import micropip; await micropip.install('signchart')");
-            }
+
+            // Install custom packages using micropip
+            await installPackages(pyodide, pypiPackages);
 
             console.log("Packages loaded:", packages);
             postMessage(JSON.stringify({ type: 'packagesLoaded', packageRequestId }));
@@ -203,6 +190,7 @@ plt.show = lambda: show_override("\${messageId}")
         this.worker.onmessage = this.handleMessage.bind(this);
         this.worker.onerror = this.handleError.bind(this);
 
+        // Send preloadPackages with the init message!
         this.worker.postMessage({ type: 'init', preloadPackages: this.preloadPackages });
     }
 
@@ -214,15 +202,12 @@ plt.show = lambda: show_override("\${messageId}")
         const packagesToLoad = packages.filter(pkg => !this.loadedPackages.has(pkg));
 
         if (packagesToLoad.length === 0) {
-            // All packages are already loaded
-            return Promise.resolve();
+            return Promise.resolve(); // All packages already loaded
         }
 
-        // Create a unique ID for this package load request
         const packageRequestId = 'pkg-' + Math.random().toString(36).substr(2, 9);
 
         return new Promise((resolve, reject) => {
-            // Store the resolve and reject functions
             this.packageLoadPromises[packageRequestId] = { resolve, reject, packages: packagesToLoad };
             this.worker.postMessage({ type: 'loadPackage', packages: packagesToLoad, packageRequestId });
         });
@@ -235,7 +220,7 @@ plt.show = lambda: show_override("\${messageId}")
         return messageId;
     }
 
-    handleMessage(event) {
+      handleMessage(event) {
         let data;
         try {
             data = JSON.parse(event.data);
@@ -256,7 +241,6 @@ plt.show = lambda: show_override("\${messageId}")
         } else if (packageRequestId && this.packageLoadPromises[packageRequestId]) {
             const packagePromise = this.packageLoadPromises[packageRequestId];
             if (data.type === 'packagesLoaded') {
-                // Mark packages as loaded
                 for (const pkg of packagePromise.packages) {
                     this.loadedPackages.add(pkg);
                 }
@@ -266,23 +250,9 @@ plt.show = lambda: show_override("\${messageId}")
             }
             delete this.packageLoadPromises[packageRequestId];
         } else {
-            // Handle messages without messageId, like 'initReady'
             if (data.type === 'initReady') {
                 console.log("Worker initialization message:", data.type);
-                // Now load preloadPackages if any
-                if (this.preloadPackages && this.preloadPackages.length > 0) {
-                    this.loadPackages(this.preloadPackages)
-                        .then(() => {
-                            console.log("Preload packages loaded:", this.preloadPackages);
-                            this.workerReadyResolve();
-                        })
-                        .catch((err) => {
-                            console.error("Failed to load preload packages:", err);
-                            this.workerReadyReject(err);
-                        });
-                } else {
-                    this.workerReadyResolve();
-                }
+                this.workerReadyResolve(); // Resolve *after* preloading is done in the worker.
             } else {
                 console.warn("Unhandled message from worker:", data);
             }
@@ -302,7 +272,6 @@ plt.show = lambda: show_override("\${messageId}")
             this.worker = null;
         }
 
-        // Reset loaded packages and create a new workerReadyPromise
         this.loadedPackages = new Set();
         this.workerReadyPromise = new Promise((resolve, reject) => {
             this.workerReadyResolve = resolve;
