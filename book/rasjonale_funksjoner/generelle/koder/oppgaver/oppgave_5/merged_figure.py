b4916597dabde49ff8d1be00c3219375e1d3a9d5
reneaas <reneaas@uio.no>
2025-02-21 02:27:17 +0100

    bugfix og ny template for å merge flere figurer automatisk

---
+++ book/rasjonale_funksjoner/generelle/koder/oppgaver/oppgave_5/merged_figure.py
@@ -0,0 +1,52 @@
+from svgutils.compose import Figure, SVG
+
+
+def main(dirname):
+
+    fignames = ["A.svg", "B.svg", "C.svg", "D.svg"]
+    # figure_paths = [dirname + path for path in figure_paths]
+    figure_paths = [dirname + "/" + figname for figname in fignames]
+    svgs = [SVG(path) for path in figure_paths]
+
+    svg_width = max(svg.width for svg in svgs)
+    svg_height = max(svg.height for svg in svgs)
+
+    fig = Figure(
+        svg_width * 2,
+        svg_height * 2,
+        SVG(dirname + "/A.svg").scale(1.25).move(0, 0),
+        SVG(dirname + "/B.svg").scale(1.25).move(svg_width, 0),
+        SVG(dirname + "/C.svg").scale(1.25).move(0, svg_height),
+        SVG(dirname + "/D.svg").scale(1.25).move(svg_width, svg_height),
+    )
+
+    fig.save(dirname + "/merged_figure.svg")
+    # fig.save("./merged_figure.svg")
+
+    # if save:
+    #     fname = __file__.split("/")[-1].replace(".py", ".svg")
+    #     plotmath.savefig(
+    #         dirname=dirname,
+    #         fname=fname,
+    #     )  # Lagrer figuren i `dirname`-directory
+
+    # if not save:
+    #     plotmath.show()
+
+
+if __name__ == "__main__":
+
+    import pathlib
+
+    # Get the directory where the script is located
+    current_dir = str(pathlib.Path(__file__).resolve().parent)
+
+    parts = current_dir.split("/")
+    for i in range(len(parts)):
+        if parts[~i] == "koder":
+            parts[~i] = "figurer"
+            break
+
+    dirname = "/".join(parts)
+
+    main(dirname=dirname)
